b51f398a4d90d332e09626e2bd569efdcebd6b37
Kalyanimuthu <kalyaniram@gmail.com>
2025-10-31 12:39:12 +0530

    Add React project folder

---
+++ react/shipping-address/src/App.jsx
@@ -0,0 +1,11 @@
+import ShippingAddressForm from './components/ShippingAddressForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-green-100 p-6">
+      <ShippingAddressForm />
+    </div>
+  );
+}
+
+export default App;
